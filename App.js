26bf7f918064a5de737c84efa239b13885100809
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-06-22 13:07:16 -0300

    Import Firebase into project

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAnpKgY_PcSbFQXPvJN0TpCfjT_WH9jh6M',
+      authDomain: 'auth-f0dad.firebaseapp.com',
+      databaseURL: 'https://auth-f0dad.firebaseio.com',
+      projectId: 'auth-f0dad',
+      storageBucket: 'auth-f0dad.appspot.com',
+      messagingSenderId: '425681366057'
+  });
+  }
+
   render() {
     return (
       <View style={styles.container}>
