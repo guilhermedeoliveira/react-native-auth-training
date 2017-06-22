fed14f59f5ee4dbe36a04692c69c4e5e157a5e3a
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-06-22 15:21:27 -0300

    Finish LoginForm styling and capabilites

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
 
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '', password: '' };
 
   render() {
     <Card>
       <CardSection>
-        <TextInput
-          value={this.state.text}
-          onChangeText={text => this.setState({ text })}
+        <Input
+          placeholder="user@gmail.com"
+          label="Email"
+          value={this.state.email}
+          onChangeText={email => this.setState({ email })}
         />
       </CardSection>
 
-      <CardSection></CardSection>
+      <CardSection>
+        <Input
+          secureTextEntry
+          placeholder="password"
+          label="Password"
+          value={this.state.password}
+          onChangeText={password => this.setState({ password })}
+        />
+      </CardSection>
 
       <CardSection>
         <Button>
