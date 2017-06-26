fc4bbe226e5eec0f0b56ea99b0e4b015f8172023
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-06-26 14:48:53 -0300

    Handle Firebase authentication when click on button

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,37 +1,57 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
+
+  onPressButton() {
+    const { email, password } = this.state;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed.' });
+          });
+      });
+  }
 
   render() {
-    <Card>
-      <CardSection>
-        <Input
-          placeholder="user@gmail.com"
-          label="Email"
-          value={this.state.email}
-          onChangeText={email => this.setState({ email })}
-        />
-      </CardSection>
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            placeholder="user@gmail.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
 
-      <CardSection>
-        <Input
-          secureTextEntry
-          placeholder="password"
-          label="Password"
-          value={this.state.password}
-          onChangeText={password => this.setState({ password })}
-        />
-      </CardSection>
+        <CardSection>
+          <Input
+            secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
 
-      <CardSection>
-        <Button>
-          Log In
-        </Button>
-      </CardSection>
-    </Card>
+        <Text>
+          {this.state.error}
+        </Text>
+
+        <CardSection>
+          <Button onPress={this.onPressButton.bind(this)}>
+            Log In
+          </Button>
+        </CardSection>
+      </Card>
+    );
   }
 }
 
